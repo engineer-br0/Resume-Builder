14f27ad32c1db870b6086f7f841097c4f54a15de
engineer-br0 <106507655+engineer-br0@users.noreply.github.com>
2022-09-21 09:09:03 +0530

    start

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Body from "./components/Body";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Header /> */}
+      <Body />
+    </div>
+  );
+}
+
+export default App;
